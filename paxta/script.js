dce1b4f9e0a82c5b5125b1d3c5b13bd1bf01b5b9
Majuhzinhah <mariajuliamartinsvieira@gmail.com>
2025-05-26 11:06:07 -0300

    Update script.js
    teste

--- paxta/script.js
+++ paxta/script.js
@@ -1,1 +1,16 @@
+const form = document.getElementById('formulario');
+const mensagem = document.getElementById('mensagem');
 
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const senha = document.getElementById('senha').value;
+
+  if (senha.length < 6) {
+    alert('A senha precisa ter pelo menos 6 caracteres!');
+    return;
+  }
+
+  mensagem.style.display = 'block';
+  form.reset();
+});
